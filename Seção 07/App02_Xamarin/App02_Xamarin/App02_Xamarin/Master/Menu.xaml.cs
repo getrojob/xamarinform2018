7d6a9b38b4bdbe818cfa3b1f26aba2886d51e622
Get Rodriguez <getrodriguez@LIVE.COM>
2018-04-14 20:39:15 -0300

    subindo projeto 2

---
+++ Seção 07/App02_Xamarin/App02_Xamarin/App02_Xamarin/Master/Menu.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_Xamarin.Master
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Menu : MasterDetailPage
+	{
+		public Menu ()
+		{
+			InitializeComponent ();
+		}
+        private void GopPaginaPerfil1(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Pages.Perfil1());
+        }
+        private void GopPaginaXamarin(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Pages.Xamarin());
+        }
+
+    }
+}
