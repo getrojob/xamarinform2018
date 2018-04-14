5009d6abb8a6297ff78e9ce91a221966261a12fa
Get Rodriguez <getrodriguez@LIVE.COM>
2018-04-14 13:08:17 -0300

    Subindo seção 07

---
+++ Seção 07/App01_LayoutXF/App01_LayoutXF/App01_LayoutXF/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App01_LayoutXF
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void GoPaginaStack(object send, EventArgs arg)
+        {
+            Navigation.PushAsync(new Layouts.Stack.StackPage());
+        }
+        private void GoPaginaGrid(object send, EventArgs arg)
+        {
+            Navigation.PushAsync(new Layouts.Grid.GridPage());
+        }
+        private void GoPaginaAbsolute(object send, EventArgs arg)
+        {
+            Navigation.PushAsync(new Layouts.Absolute.AbsolutePage());
+        }
+        private void GoPaginaRelative(object send, EventArgs arg)
+        {
+            Navigation.PushAsync(new Layouts.Relative.RelativePage());
+        }
+        private void GoPaginaScroll(object send, EventArgs arg)
+        {
+            Navigation.PushAsync(new Layouts.Scroll.ScrollPage());
+        }
+
+
+
+
+
+    }
+}
