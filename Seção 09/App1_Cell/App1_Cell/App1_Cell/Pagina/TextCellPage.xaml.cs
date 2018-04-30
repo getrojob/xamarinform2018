9eaaac2f4913569baf8e9160775440cca9f5e714
Get Rodriguez <getrodriguez@LIVE.COM>
2018-04-29 23:43:49 -0300

    subindo o projeto seção 9

---
+++ Seção 09/App1_Cell/App1_Cell/App1_Cell/Pagina/TextCellPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App1_Cell.Modelo;
+
+
+namespace App1_Cell.Pagina
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TextCellPage : ContentPage
+	{
+		public TextCellPage ()
+		{
+			InitializeComponent ();
+
+            List<Funcionario> Lista = new List<Funcionario>();
+
+
+		}
+	}
+}
