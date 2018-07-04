c0b346844f2aca8ef38cc01b6c3da66a1e1e3ebe
Get Rodriguez <getrodriguez@LIVE.COM>
2018-07-03 21:31:34 -0300

    atualizando projeto

--- Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/InicioViewModel.cs
+++ Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/InicioViewModel.cs
@@ -4,6 +4,7 @@ using System.Text;
 using App1_Mimica.Model;
 using Xamarin.Forms;
 using System.ComponentModel;
+using App1_Mimica.Armazenamento;
 
 namespace App1_Mimica.ViewModel
 {
@@ -19,6 +20,8 @@ namespace App1_Mimica.ViewModel
 
         private void IniciarJogo()
         {
+            Armazenamento.Armazenamento.Jogo = this.Jogo;
+            Armazenamento.Armazenamento.RodadaAtual = 1;
             App.Current.MainPage = new View.Jogo();
         }
         public event PropertyChangedEventHandler PropertyChanged;
--- Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/JogoViewModel.cs
+++ Seção 12/App1_Mimica/App1_Mimica/App1_Mimica/ViewModel/JogoViewModel.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
 
 namespace App1_Mimica.ViewModel
 {
-    class JogoViewModel
+    public class JogoViewModel
     {
+        public byte PalavraPontuacao { get; set; }
+        public string Palavra { get; set; }
+        public bool ContainerContagem { get; set; }
+        public bool ContainerIniciar { get; set; }
+
+        public Command MostrarPalavra { get; set; }
+        public Command Acertou { get; set; }
+        public Command Errou { get; set; }
+        public Command Iniciar { get; set; }
+
+        public JogoViewModel()
+        {
+            ContainerContagem = false;
+
+            MostrarPalavra = new command();
+            Acertou = new Command();
+            Errou = new Command();
+            Iniciar = new Command();
+
+        }
     }
 }
